a91575d5d340f34a316a837d1012a56ff10305c3
Ken McKenzie <kenneth.mckenzie@myacuity.com>
2023-02-06 08:11:55 -0500

    Initial commit of legacy database mapping

---
+++ src/main/java/ken/learning/sdjpalegacydatabase/SdjpaLegacyDatabaseApplication.java
@@ -0,0 +1,13 @@
+package ken.learning.sdjpalegacydatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaLegacyDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdjpaLegacyDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ken/learning/sdjpalegacydatabase/SdjpaLegacyDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ken.learning.sdjpalegacydatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaLegacyDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
